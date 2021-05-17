6570f246da399dfeb336090623a9145e46ba7d25
Marcell <mpather47@gmail.com>
2021-05-17 08:55:23 +0200

    first commit

---
+++ EmployeeManager.ViewModel/Command/DelegateCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace EmployeeManager.ViewModel.Command
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action execute;
+        private readonly Func<bool> canExecute;
+
+        public DelegateCommand(Action execute, Func<bool> canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null
+                ? true
+                : canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            execute();
+        }
+    }
+}
